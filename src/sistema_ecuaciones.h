d331b12e00daf6256774b2775fbf76f5808bf3b9
Martin Jedwabny <martiniedva@gmail.com>
2015-08-21 19:04:30 -0300

    Pasando las cosas de map a vector

--- src/sistema_ecuaciones.h
+++ src/sistema_ecuaciones.h
@@ -1,8 +1,9 @@
 #ifndef SISTEMA_ECUACIONES_H_
 #define SISTEMA_ECUACIONES_H_
 
+enum tipoResolucion : bool {GAUSS, LU};
+
 #include <iostream>
-#include <map>
 #include <vector>
 
 using namespace std;
@@ -10,16 +11,15 @@ using namespace std;
 class SistemaEcuaciones{
 	public:
 		SistemaEcuaciones();
-		SistemaEcuaciones(vector<vector<double> > matrizA, map<int, vector<double> > instBi, int dimMatriz);
+		SistemaEcuaciones(vector<vector<double> > A, map<int, vector<double> > instBi, int dimMatriz);
 		vector<double> resolverSistema(int instancia, bool lu);
 	private:
-		vector<vector<double> > matrizA;
-		map<int, vector<double> > instB;
+		vector<vector<double> > A;
+		vector<vector<double> > instB;
 		int dimMatriz;
-		void eliminacionGaussiana(vector<vector<double> > &matrizA, vector<double> &b, int n);
-		vector<double> resolverTriangular(vector<vector<double> > &matrizU, vector<double> &b, int n);
+		void eliminacionGaussiana(vector<vector<double> > &A, vector<double> &b, int n);
+		vector<double> resolverTriangular(vector<vector<double> > &U, vector<double> &b, int n);
 		void imprimirSistema(vector<vector<double> > &mA, vector<double> &b );
-
 };
 
 #endif // SISTEMA_ECUACIONES_H_INCLUDED
